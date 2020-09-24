d023aac6807ce2d7c035a3279f75d30d9a3caeab
Rimmkin <70262551+Rimmkin@users.noreply.github.com>
2020-09-24 20:14:02 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
         int price = 7800;
-        int miles = (int) service.calculate(7800);
+        int miles = (int) service.calculate(price);
         System.out.println(miles);
     }
 }
